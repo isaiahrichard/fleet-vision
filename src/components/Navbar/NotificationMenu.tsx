0205c889e05f48df69e44c34ccd27cbb35f6f73f
Isaiah Richards <isaiah@tablz.com>
2024-07-16 20:31:27 -0400

    update

--- src/components/Navbar/NotificationMenu.tsx
+++ src/components/Navbar/NotificationMenu.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import styles from "./navbar.module.css";
 import { useRouter, usePathname } from "next/navigation";
 import { Notifications, Settings, Person } from "@mui/icons-material";
@@ -9,8 +9,29 @@ export const NotificationMenu = () => {
   const router = useRouter();
   const pathName = usePathname();
 
-  const { count, notifications } = useNotificationContext();
-  const [modalOpen, setModalOpen] = useState(false);
+  const { notifications, addNotification, deleteNotification, count } =
+    useNotificationContext();
 
-  return <Box className={styles.notifMenu}>TEST TEST TEST TEST TEST</Box>;
+  const notifColour = (notifType: "error" | "warn" | "info") => {
+    if (notifType == "error") return "red";
+    else if (notifType == "warn") return "yellow";
+    return "green";
+  };
+
+  return (
+    <Box className={styles.notifMenu}>
+      {count ? (
+        <Box>
+          {notifications.map((notif) => (
+            <Box bgcolor={notifColour(notif.type)}>
+              <Typography>{notif.message}</Typography>
+              <Typography>{notif.originPage}</Typography>
+            </Box>
+          ))}
+        </Box>
+      ) : (
+        <Box>No Notifications</Box>
+      )}
+    </Box>
+  );
 };
