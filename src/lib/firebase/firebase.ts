78fb0e28875e49c4f0a6f10480e900b1de549d9c
Isaiah Richards <r.isaiah3415@gmail.com>
2025-01-29 01:15:10 -0500

    remove sensitive info

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -5,13 +5,6 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCVRQhnulHWs4mTrMX8gA4vMrCJhvmcqiM",
-  authDomain: "fleet-vision.firebaseapp.com",
-  projectId: "fleet-vision",
-  storageBucket: "fleet-vision.firebasestorage.app",
-  messagingSenderId: "1024634134751",
-  appId: "1:1024634134751:web:125038ae32763fb578496a",
-  measurementId: "G-PLMQ6XDTCC"
 };
 
 
